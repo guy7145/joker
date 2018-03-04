3fa0278b98e822e141898a9dfff1a801318003b4
guydani <guy7145@gmail.com>
2018-03-04 13:15:38 +0200

    hmm?

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 import cv2
 import os
-
 import joker
 
 
 def main():
-    dir = '.\\cards'
-    j = joker.JokerToolbox(dir + r'\templates', dir + r'\final cards')
+    root_dir = '.\\cards'
+    j = joker.JokerToolbox(root_dir + r'\templates', root_dir + r'\final cards')
     for d, t in zip(['characters', 'enemies', 'spells', 'supporters', 'tools'],
                     ['character', 'enemy', 'spell', 'supporter', 'tool']):
-        print(dir + r'\images' + '\\' + d)
-        j.edit_dir(dir + r'\images' + '\\' + d, t)
+        print(root_dir + r'\images' + '\\' + d)
+        j.edit_dir(root_dir + r'\images' + '\\' + d, t)
         print('done')
     return
 
